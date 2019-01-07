1d0a7d05436972fba57ca078c390d966b746da31
Sarah Barron <s.barron2338@gmail.com>
2019-01-07 16:54:05 -0600

    eod commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,46 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello,");
-        System.out.print(" World!");
+        System.out.println(" World!");
+
+        int myFavoriteNumber = 22;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Clever Girl";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        Rewrite the following expressions using the relevant shorthand assignment operators:
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+        byte ohYeah = 127;
+        ohYeah++;
+        System.out.println(ohYeah);
     }
 }
