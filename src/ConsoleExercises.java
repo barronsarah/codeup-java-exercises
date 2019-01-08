466aaf30abc3d72bebb0702fceedbac08b8b5e9f
Sarah Barron <s.barron2338@gmail.com>
2019-01-07 22:41:36 -0600

    eod work

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,10 +6,25 @@ public class ConsoleExercises {
 
         System.out.format("The value of pi is approximately %.2f.%n", pi);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int UserNumber = scanner.next();
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int UserNumber;
+        UserNumber = scan.nextInt();
+        System.out.println("You entered " + UserNumber);
+
+        System.out.println("Enter 3 words separated by a space ");
+        String wordOne, wordTwo, wordThree;
+        wordOne = scan.next();
+        wordTwo = scan.next();
+        wordThree = scan.next();
+        System.out.println(wordOne);
+        System.out.println(wordTwo);
+        System.out.println(wordThree);
+
+        System.out.print("Enter a sentence: ");
+        String userInput = scan.nextLine();
+        System.out.format(userInput);
 
     }
 }
