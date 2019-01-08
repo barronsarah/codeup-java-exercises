8208166a95c0f66766b79063098d088f739637e7
Sarah Barron <s.barron2338@gmail.com>
2019-01-08 14:24:52 -0600

    FizzBuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,31 +2,24 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main (String[] args){
 
-        int i = 5;
-        while (i <= 15){
-            System.out.println(i);
-            i++;
+//        FizzBuzz exercise
+        for(int i=1; i<=100; i++){
+            if((i%3 == 0) && (i%5 == 0)){
+                System.out.println("FizzBuzz");
+                continue;
+            }
+            if(i%3 == 0){
+                System.out.println("Fizz");
+                continue;
+            }
+            if(i%5 == 0){
+                System.out.println("Buzz");
+                continue;
+            }
+             else {
+                System.out.println(i);
+            }
         }
 
-//        int j = 100;
-//        do {
-//            System.out.println(j);
-//            j-= 5;
-//        }while(j >= -10);
-    //same function rewritten as a for loop
-        for(int j = 100; j >= -10; j-=5){
-            System.out.println(j);
-        }
-
-
-//        long e = 2;
-//        do{
-//            System.out.println(e);
-//            e *= e;
-//        }while(e < 1000000);
-        //same function rewritten as a for loop
-        for(long e =2; e < 1000000; e*=){
-            System.out.println(e);
-        }
     }
 }
